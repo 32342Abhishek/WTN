59a6e0f43ae3b7eb897d5ca4ff0a0d5fb24b113c
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2026-06-29 16:48:44 +0530

    Create ArrayListDemo.java

---
+++ Milestone 1/Wrapper Classes/ArrayListDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+        list.add(100);
+        list.add(200);
+        list.add(300);
+
+        System.out.println(list);
+    }
+}
