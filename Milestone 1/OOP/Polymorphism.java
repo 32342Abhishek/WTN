88c375dd0cace97bf3f7c134c80f8d60ed286237
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2026-06-29 16:42:32 +0530

    Create Polymorphism.java

---
+++ Milestone 1/OOP/Polymorphism.java
@@ -0,0 +1,22 @@
+class Addition{
+
+    int add(int a,int b){
+        return a+b;
+    }
+
+    int add(int a,int b,int c){
+        return a+b+c;
+    }
+}
+
+class Polymorphism{
+
+    public static void main(String args[]){
+
+        Addition obj=new Addition();
+
+        System.out.println(obj.add(10,20));
+
+        System.out.println(obj.add(10,20,30));
+    }
+}
