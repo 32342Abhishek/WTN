7b2c7b3c4f1745903b5f91b292caa64644b80a1d
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2026-06-29 16:47:30 +0530

    Create WrapperClasses.java

---
+++ Milestone 1/Wrapper Classes/WrapperClasses.java
@@ -0,0 +1,15 @@
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        int num=10;
+
+        Integer obj=num;
+
+        System.out.println(obj);
+
+        int value=obj;
+
+        System.out.println(value);
+    }
+}
