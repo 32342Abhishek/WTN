5b2ef7898a6d3c2da8321b1b5143fa1baaf9a4d9
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2026-06-29 16:35:22 +0530

    Create Arr.java

---
+++ Milestone 1/Arrays/Arr.java
@@ -0,0 +1,9 @@
+class Arr {
+    public static void main(String[] args) {
+
+        int[] regNo = {978,100,345,785};
+
+        for(int i=0;i<regNo.length;i++)
+            System.out.println(regNo[i]);
+    }
+}
