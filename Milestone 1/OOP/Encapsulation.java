fd37b44a546fc0f4c91331bbe1de9b4ecf398a4a
Abhishek Kumar <157559756+32342Abhishek@users.noreply.github.com>
2026-06-29 16:40:24 +0530

    Create Encapsulation.java

---
+++ Milestone 1/OOP/Encapsulation.java
@@ -0,0 +1,24 @@
+class Employee{
+
+    private int salary;
+
+    public void setSalary(int salary){
+        this.salary=salary;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+}
+
+class Encapsulation{
+
+    public static void main(String args[]){
+
+        Employee e=new Employee();
+
+        e.setSalary(50000);
+
+        System.out.println(e.getSalary());
+    }
+}
